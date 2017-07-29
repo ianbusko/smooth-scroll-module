0387be22af0123dd694ccb5619c0555c7c9f5cc4
Ian Busko <ian.busko@gmail.com>
2017-07-28 21:33:46 -0400

    add initial gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var gulp = require('gulp');
+
+gulp.task('build', function () {
+
+});
+
+gulp.task('watch', ['build'], function () {
+    gulp.watch('./src/js/*.js', ['build']);
+});
+
+gulp.task('default', ['build', 'watch']);
