7e8355b84a9d548a20f487de2103d6f066547c9b
Ian Busko <ian.busko@gmail.com>
2017-07-29 13:08:13 -0400

    add gulp-babel
    test out new babel plugin in build
    
    update packages to use babel, minify, and rename
    
    remove unused dependencies
    
    remove map
    
    correct error in package.json
    
    only minify unminified files
    
    rename output files to scroll-module

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,25 @@
 var gulp        = require('gulp');
-var browserify  = require('browserify');
-var babelify    = require('babelify');
-var source      = require('vinyl-source-stream');
-var buffer      = require('vinyl-buffer');
 var uglify      = require('gulp-uglify');
-var sourcemaps  = require('gulp-sourcemaps');
-var livereload  = require('gulp-livereload');
+var rename = require('gulp-rename');
+var babel = require('gulp-babel');
+
 
 gulp.task('build', function () {
-    return browserify({entries: './src/app.js', debug: true})
-        .transform("babelify", { presets: ["es2015"] })
-        .bundle()
-        .pipe(source('app.min.js'))
-        .pipe(buffer())
-        .pipe(sourcemaps.init())
-        .pipe(uglify())
-        .pipe(sourcemaps.write('./maps'))
-        .pipe(gulp.dest('./dist'))
-        .pipe(livereload());
+  return gulp.src('src/*.js')
+    .pipe(babel({
+        presets: ['es2015']
+    }))
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task('watch', ['build'], function () {
+gulp.task('minify', function(cb){
+  gulp.src(['dist/*.js', '!dist/*.min.js'])
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('watch', ['build', 'minify'], function () {
     livereload.listen();
     gulp.watch('./src/*.js', ['build']);
 });
