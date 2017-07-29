990bbf99dc08d882b451f40566934b7c8d19f7cc
Ian Busko <ian.busko@gmail.com>
2017-07-28 21:35:31 -0400

    move module into src folder
    add babel and browserify dependencies
    
    update gulp tasks for browserify and babel
    
    rename smooth-scroll-module.js to app.js
    
    update gulpfile to work with project
    
    add dist output
    
    install dependencies for minifying javascript
    
    add extra gulp tasks to minify output
    
    add final dependencies to output minified javascript

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,28 @@
-'use strict';
-
-var gulp = require('gulp');
+var gulp        = require('gulp');
+var browserify  = require('browserify');
+var babelify    = require('babelify');
+var source      = require('vinyl-source-stream');
+var buffer      = require('vinyl-buffer');
+var uglify      = require('gulp-uglify');
+var sourcemaps  = require('gulp-sourcemaps');
+var livereload  = require('gulp-livereload');
 
 gulp.task('build', function () {
-
+    return browserify({entries: './src/app.js', debug: true})
+        .transform("babelify", { presets: ["es2015"] })
+        .bundle()
+        .pipe(source('app.min.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./dist'))
+        .pipe(livereload());
 });
 
 gulp.task('watch', ['build'], function () {
-    gulp.watch('./src/js/*.js', ['build']);
+    livereload.listen();
+    gulp.watch('./src/*.js', ['build']);
 });
 
 gulp.task('default', ['build', 'watch']);
